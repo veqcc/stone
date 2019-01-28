22615ed55714b3525dc7d5e61f0cd9fce6cfe632
神戸隆太 <kambe3141@gmail.com>
2019-01-28 21:59:19 +0900

    fix indent

--- src/chap6/BasicInterpreter.java
+++ src/chap6/BasicInterpreter.java
@@ -7,14 +7,13 @@ public class BasicInterpreter {
     public static void main(String[] args) throws ParseException {
         run(new BasicParser(), new BasicEnv());
     }
-    public static void run(BasicParser bp, Environment env)
-        throws ParseException
-    {
+
+    public static void run(BasicParser bp, Environment env) throws ParseException {
         Lexer lexer = new Lexer(new CodeDialog());
         while (lexer.peek(0) != Token.EOF) {
             ASTree t = bp.parse(lexer);
             if (!(t instanceof NullStmnt)) {
-                Object r = ((BasicEvaluator.ASTreeEx)t).eval(env);
+                Object r = ((BasicEvaluator.ASTreeEx) t).eval(env);
                 System.out.println("=> " + r);
             }
         }
