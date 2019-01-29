d453d211bab4001e6665bf431d27971a1cbe487b
神戸隆太 <kambe3141@gmail.com>
2019-01-29 15:53:51 +0900

    chap7 remove gluonj from Closure

--- src/stone/ClosureInterpreter.java
+++ src/stone/ClosureInterpreter.java
@@ -3,15 +3,15 @@ package stone;
 import stone.ast.ASTree;
 import stone.ast.NullStmnt;
 
-public class FuncInterpreter {
+public class ClosureInterpreter {
     public static void main(String[] args) throws ParseException {
-        run(new FuncParser(), new NestedEnv());
+        run(new ClosureParser(), new NestedEnv());
     }
 
-    public static void run(FuncParser fp, NestedEnv env) throws ParseException {
+    public static void run(ClosureParser cp, NestedEnv env) throws ParseException {
         Lexer lexer = new Lexer(new CodeDialog());
         while (lexer.peek(0) != Token.EOF) {
-            ASTree t = fp.parse(lexer);
+            ASTree t = cp.parse(lexer);
             if (!(t instanceof NullStmnt)) {
                 Object r = t.eval(env);
                 System.out.println("=> " + r);
